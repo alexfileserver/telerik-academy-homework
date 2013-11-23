d6887774b6825e3e47cc304ea80af70ba34c51c1
atipetrov <ati_petrov@abv.bg>
2013-11-23 23:02:50 +0200

    Revert "Adding the first files !"
    This reverts commit 3358034bcc40c0a69d0cc01ebbe1c855d4ba96c1.

--- Telerik-Academy-Homework/script/3bit.js
+++
@@ -1,8 +0,0 @@
-function FindThirdBit()
- {
-    var number = parseInt(document.getElementById("textIn").value);
-    var mask = 1 << 3;
-    var numberAndMask = number & mask;
-    numberAndMask = numberAndMask >> 3;
-    document.getElementById("textOut").value = numberAndMask;
-  }
--- Telerik-Academy-Homework/script/5 and 7.js
+++
@@ -1,9 +0,0 @@
-function OddOrEven() {
-            var number = parseInt(document.getElementById("textIn").value);
-            if (number % 5 == 0 && number % 7 == 0) {
-                document.getElementById("textOut").value = "Divides without remainder.";
-            }
-            else {
-                document.getElementById("textOut").value = "Divides with remainder.";
-            }
-        }
